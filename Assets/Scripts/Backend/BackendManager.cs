7797d307326351109fd9d17541a77fb9d7340ae1
2019147519 <songsy0418@gmail.com>
2026-05-14 10:40:28 +0900

    아바타 수정사항 적용

--- Assets/Scripts/Backend/BackendManager.cs
+++ Assets/Scripts/Backend/BackendManager.cs
@@ -36,10 +36,6 @@ public class BackendManager : MonoBehaviour
         var bro = Backend.Initialize();
         IsInitialized = bro.IsSuccess();
 
-#if UNITY_ANDROID && !UNITY_EDITOR
-    Debug.Log("[BackendManager] GoogleHash: " + Backend.Utils.GetGoogleHash());
-#endif
-
         Report(IsInitialized ? "Backend initialized." : $"Backend initialization failed: {bro}");
         return IsInitialized;
     }
