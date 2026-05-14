47e47937c829a910206f3257d4fb7a6268015168
Yoon Taekyu <apcocoa2019@gmail.com>
2026-05-14 10:58:12 +0900

    종료 버튼

--- Assets/Scripts/UI/MainMenuUI.cs
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -142,4 +142,13 @@ public class MainMenuUI : MonoBehaviour
     {
         ShowOnly(_gameModePanel);
     }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
 }
